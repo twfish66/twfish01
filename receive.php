7c5741cc8660f3a8811f58aa24c17bee132326ed
twfish66 <32149424+twfish66@users.noreply.github.com>
2018-01-09 11:36:22 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -1,4 +1,10 @@
 # twfish01
 <?php
-echo " hello how are you!! "
+$json_str = file_get_contents('php://input'); //接收request的body
+$json_obj = json_decode($json_str);//轉JSON格式
+
+$myfile = fopen("log.txt","w+") or die("Unable to open file");
+fwrite($myfile, "\xEF\xBB\xBF".$json_str);
+fclose($myfile)
+
 ?>
