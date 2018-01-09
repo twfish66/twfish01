9b136de33ebeb34147ae9da8d507aa552a69441b
twfish66 <32149424+twfish66@users.noreply.github.com>
2018-01-09 15:38:37 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -112,7 +112,7 @@ $myfile = fopen("log.txt","w+") or die("Unable to open file!"); //設定一個lo
 			$ch = curl_init($url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-				'Authorization: Bearer n4mZIQp9UqWXhCEgIg1fLmyjUeDMgCe/bF+4EOBDZ7fGscOgNGFsHTr3fGco/E7A5hq7A7jiDszSCk/j3pVVPbx7nf0E+FKe5jX6syQGOxO7kwp5lmZ3zRES1qxceq/N+/E9Qy5gSDbBx56l8sScTwdB04t89/1O/w1cDnyilFU=',
+				'Authorization: Bearer /qwGugx145+ArJNNIOsltn/IxSyYPgh2x0gZHlTVi6uHvJwG4pHxpv+j2DcDeS0XFiT/mv9rfWCgTXbHf5cKeK1P6Bxt4dcoi0TGumShf0RYrxHbXYAzaXxqTZrzuDb7U3PGadttskbH27O+Ph/3bgdB04t89/1O/w1cDnyilFU=
 			));
 				
 			$json_content = curl_exec($ch);
